ead4d7301feeaa07be70e0396f0739c4412432f8
Naveen-HN <hnnaveen93@gmail.com>
2020-01-22 16:13:30 -0500

    Added Objects.js

---
+++ Objects.js
@@ -0,0 +1,19 @@
+let info = {
+    greeting: 'Hello!',
+    age: 56,
+    helpful: true,
+    favouriteColor: ['Black','Blue'],
+    name: {
+        first: 'Naveen',
+        last: 'Holali',
+    },
+    homeTown:'Bangalore',
+    car: undefined
+};
+
+console.log(info.greeting);
+console.log['greeting'];
+console.log(info.name);
+console.log("Helpful?:"+info.helpful);
+info.helpful=false;
+console.log("Helpful?:"+info.helpful);
