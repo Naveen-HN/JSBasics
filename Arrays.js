8b31da9d6d9d7ebfabfea1fe66b3e8bd4bde50b3
Naveen-HN <hnnaveen93@gmail.com>
2020-01-22 15:57:57 -0500

    Added Arrays Program

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+let colors = [];
+colors.push("Red");
+colors.push("Green");
+colors.push("Blue");
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[colors.length-1]);
+colors.pop();
+console.log(colors);
