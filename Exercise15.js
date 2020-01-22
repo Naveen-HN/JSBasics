98b67b75cb42495e11341c6d839f2e575db53568
Naveen-HN <hnnaveen93@gmail.com>
2020-01-22 17:26:21 -0500

    Checking number greater than 13 and returning absolute difference

---
+++ Exercise15.js
@@ -0,0 +1,8 @@
+var givennumber = 15;
+if(givennumber<=13){
+    console.log("The difference is:"+(13-givennumber));
+}
+else
+{
+    console.log("The absolute difference is:"+(givennumber-13)*2);
+}
