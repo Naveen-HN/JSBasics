95c1241ff081b610fd9aba5e8644e15508ffd069
Naveen-HN <hnnaveen93@gmail.com>
2020-01-26 13:32:28 -0500

    Added Properties

--- Prototypes.js
+++ Prototypes.js
@@ -8,8 +8,12 @@ Person.prototype.calculateAge = function(){
     console.log(2020-this.yearofBirth);
 }
 
+Person.prototype.LastName;
+
 var John = new Person('John',1987,'Mechanic');
 var Jane = new Person('Jane',1995,'Teacher');
 
+Jane.LastName = "Smith";
 //console.log(John.yearofBirth);
 John.calculateAge();
+console.log("Jane Last name is:"+Jane.LastName);
